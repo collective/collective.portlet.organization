fc183c418fe6cdd1ff7222ba054fd1665ef3eaa2
JeanMichel FRANCOIS <toutpt@gmail.com>
2012-09-02 15:05:03 +0200

    initial commit

---
+++ collective/portlet/organization/__init__.py
@@ -0,0 +1,6 @@
+from zope.i18nmessageid import MessageFactory
+OrganizationPortletMessageFactory = MessageFactory('collective.portlet.organization')
+
+
+def initialize(context):
+    """Initializer called when used as a Zope 2 product."""
